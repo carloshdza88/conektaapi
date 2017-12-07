91a795b8d0cf8e61dcf1d27944e6ec47bc87d2d9
carlos.h.0440 <carlos.13.01.uam@gmail.com>
2017-12-06 22:16:33 -0600

    Se agrega validación para revisar el saldo del wallet antes de enviar saldo a otro wallet

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -15,32 +15,71 @@ module Api
             # numcuentadestino (integer)
             # monto (float)
 
-             # Verificar el saldo en la cuenta de origen , 
+            # Verificar el saldo en la cuenta de origen , 
         
+             wallet_origen = Wallet.where("numcuenta = ? " , params[:numcuentaorigen])
 
-          
+             wallet_destino = Wallet.where("numcuenta = ? " , params[:numcuentadestino])
 
-             # Registrar el retiro de la cuenta origen
-             Transaction.create(numcuenta: params[:numcuentaorigen] , 
-                                 deposito: 0 , 
-                                   retiro: params[:monto] ,
-                                    saldo: 2000 , 
-                              descripcion: 'Se retira ' + params[:monto].to_s + ' para la transacción')
+             #Verificar si existe el wallet de origen
+             if wallet_origen.present?
+
+              #  render json: { data: wallet_origen , fondo: wallet_origen.fondo}
+
+               
+                #Verificar si existe el wallet destino
+                if wallet_destino.present?
+
+                    wallet_origen = wallet_origen.first
+                    wallet_destino = wallet_destino.first
+                    
+                    if wallet_origen.fondo >= params[:monto].to_f
+
+                        # Registrar el retiro de la cuenta origen
+                        Transaction.create(numcuenta: params[:numcuentaorigen] , 
+                                            deposito: 0 , 
+                                            retiro: params[:monto] ,
+                                             saldo: wallet_origen.fondo - params[:monto].to_f , 
+                                       descripcion: 'Se retira ' + 
+                                      params[:monto].to_s + ' para la transacción')
+
+                            # Registrar el deposito en la cuenta destino                   
+                        Transaction.create(numcuenta: params[:numcuentadestino] , 
+                                            deposito: params[:monto] , 
+                                              retiro: 0 ,
+                                               saldo: wallet_destino.fondo + params[:monto].to_f , 
+                                         descripcion: 'Se recibe ' + params[:monto].to_s + ' en la cuenta')
 
 
-             # Registrar el deposito en la cuenta destino                   
-             Transaction.create(numcuenta: params[:numcuentadestino] , 
-                                 deposito: params[:monto] , 
-                                   retiro: 0 ,
-                                    saldo: 4000 , 
-                              descripcion: 'Se recibe ' + params[:monto].to_s + ' en la cuenta')
+                            render json: { status: 'succes' , 
+                                message: 'Se realizo el envío existosamente de ' + 
+                                            params[:monto] + ' de la cuenta ' + 
+                                            params[:numcuentaorigen].to_s + ' a la cuenta ' + 
+                                            params[:numcuentadestino].to_s }, status: :ok  
+                                        
+                    else
+                        render json: { status: 'denied' , message: 'El wallet con número de cuenta: ' + params[:numcuentaorigen]  + ' no tiene suficiente saldo.  Saldo actual:' + wallet_origen.fondo.to_s }                                       
+                    end
+                    
 
+                else
+                    render json: { status: 'denied' , message: 'El wallet destino con el número de cuenta:' + 
+                    params[:numcuentadestino] + 
+                   ' No existe.' }
 
-             render json: { status: 'succes' , 
-                           message: 'Se realizo el envío existosamente de ' + 
-                                     params[:monto] + ' de la cuenta ' + 
-                                     params[:numcuentaorigen].to_s + ' a la cuenta ' + 
-                                     params[:numcuentadestino].to_s}, status: :ok                
+                end
+
+            else
+
+                    render json: { status: 'denied' , message: 'El wallet de origen con el número de cuenta:' + 
+                                                                params[:numcuentaorigen].to_s + 
+                                                               ' No existe. ' }
+                    
+            end
+             
+                         
+
+                         
                               
 
 
